990df35808407edbadf2a5ab50d8871737970e64
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-08-11 12:57:28 +0200

    fix

--- resources/global.d.ts
+++ resources/global.d.ts
@@ -1,6 +1,4 @@
 import { Alpine as AlpineType } from 'alpinejs'
-import { AxiosStatic } from 'axios'
-import route from 'ziggy-js'
 
 /**
  * From https://bobbyhadz.com/blog/typescript-make-types-global
@@ -9,8 +7,6 @@ declare global {
   const Alpine: AlpineType
   interface Window {
     Alpine: AlpineType
-    // axios: AxiosStatic
-    // ziggy: typeof route
   }
 }
 
