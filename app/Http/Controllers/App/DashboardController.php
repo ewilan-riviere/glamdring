7630a20298ff49b17520788ed0e29a489c3742e3
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-04-07 19:43:58 +0200

    fix

--- app/Http/Controllers/App/DashboardController.php
+++ app/Http/Controllers/App/DashboardController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\App;
 
+use App\Http\Controllers\Controller;
 use App\Models\Project;
 use App\Models\User;
 use App\Services\GitForgeService;
